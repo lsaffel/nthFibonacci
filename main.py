826949cd8a3d6f7cec46ea64276ec9edbf2c0502
Lisa <llvvss@icloud.com>
2022-07-25 22:01:41 -0600

    added comments describing the question

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# Nth Fibonacci The Fibonacci sequence is defined as follows: the first number
+# of the sequence is 0, the second number is 1, and the nth number
+# is the sum of the (n-1)th and (n-2)th numbers. Write a function that takes
+# an integer n and returns the nth Fibonacci number
+
 def getNthFib(n):
     if n == 1:
         return 0
